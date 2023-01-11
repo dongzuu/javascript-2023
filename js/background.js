bb55a4a816a79f60ded39b98114326f83467e6c8
dongzuu <dongzu1231@gmail.com>
2023-01-10 21:39:39 -0800

    dd
    ddd

--- js/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-const bgImage = document.createElement("img");
-
-const imagesBox = document.querySelector("#images-box");
-
-bgImage.src = `images/${chosenImage}`;
-
-imagesBox.appendChild(bgImage);
